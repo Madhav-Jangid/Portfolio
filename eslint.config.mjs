c2c25dd33b482308b3f5f70ca686d25d36025e22
Madhav Jangid <manumadhavjangid@gmail.com>
2025-02-18 01:46:06 +0530

    es lint off

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,11 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "no-var": "off", // Disables 'no-var' rule
+      "@typescript-eslint/no-unused-vars": "off", // Disables 'no-unused-vars' rule
+    },
+  },
 ];
 
 export default eslintConfig;
